00857c8322a80d053dc4f29b27382d319704237e
Troy McGinnis <hello@troymcginnis.com>
2018-03-11 00:30:46 -0800

    cog::params created

--- lib/params.sh
+++ lib/params.sh
@@ -3,10 +3,11 @@
 # Cog Core Functions
 # Author: Troy McGinnis
 # Company: Gearbox
-# Updated: March 6, 2018
+# Updated: March 11, 2018
 #
 # HISTORY:
 #
+# * 2018-03-11 - Params function created
 # * 2018-03-06 - First Creation
 #
 # TODO:
@@ -34,4 +35,36 @@ cog::params_merge() {
 #
 cog::params_require() {
   message "Nothing to see here..."
+}
+
+# Params
+# An easier way to take in parameters
+#
+# @arg $1 Array of arguments
+#
+cog::params() {
+  if [[ -z "$1" ]]; then
+    error "Please provide a list of params."
+  fi
+
+  if [[ -z "$2" ]]; then
+    error "Please provide $@."
+  fi
+
+  # Build the case statement
+  cases=($1)
+  case="case \$i in "
+
+  # Loop the provided params
+  for i in "${cases[@]}"
+  do
+    case="${case}--${i}=*) ${i}=\"\${i#*=}\";; "
+  done
+  case="${case}esac"
+
+  # Extract those params
+  for i in "$@"
+  do
+    eval "$case"
+  done
 }
