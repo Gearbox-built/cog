a316c058a16797caff6d1406f58ee6aec1d64e39
Troy McGinnis <hello@troymcginnis.com>
2018-04-04 11:04:57 -0700

    Removed non-cog related requirements

--- lib/core.sh
+++ lib/core.sh
@@ -31,7 +31,7 @@ cog::check_requirement() {
 # Performs a check on cog's basic needs
 #
 cog::check_base_requirements() {
-  local requirements; requirements=(npm yarn bower rvm bpkg)
+  local requirements; requirements=(rvm bpkg)
 
   for i in "${requirements[@]}"; do
     cog::check_requirement "${i}"
